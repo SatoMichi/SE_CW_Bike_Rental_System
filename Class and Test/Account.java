681e7817d341a3a633ff8a18b6b14e6ebdc440b2
Zspirates96 <32324812+Zspirates96@users.noreply.github.com>
2019-11-23 00:39:53 +0000

    Add files via upload

---
+++ Class and Test/Account.java
@@ -0,0 +1,24 @@
+
+public class Account {
+	
+	private String name;
+	private Location address;
+	private int phone;
+	private String email;
+	
+	public Account(String n, Location a, int p, String e) {
+		this.name = n;
+		this.address = a;
+		this.phone = p;
+		this.email = e;
+	}
+	
+	public String getName() { return this.name;}
+	public void setName(String n) { this.name = n;}
+	public Location getAddress() { return this.address;}
+	public void setAddress(Location a) { this.address = a;}
+	public int getPhone() { return this.phone;}
+	public void setPhone(int p) { this.phone = p;}
+	public String getEmail() { return this.email;}
+	public void setEmail(String e) { this.email = e;}
+}
--- Class and Test/BikeProvider.java
+++ Class and Test/BikeProvider.java
@@ -1,26 +1,15 @@
 import java.util.List;
 
-public class BikeProvider {
+public class BikeProvider extends Account {
 	private String openingHours;
 	private List<BikeProvider> partners;
-	private String name;
-	private Location address;
-	private int phone;
-	private String email;
 	
 	public BikeProvider(String name, String email, int phone, Location address, String openingHours) {
-		this.name = name;
-		this.email = email;
-		this.phone = phone;
-		this.address = address;
+		super(name, address, phone, email);
 		this.openingHours = openingHours;
 	}
 	
 	public void addPartner(BikeProvider p) { partners.add(p);}
-	public String getName() { return this.name;}
-	public String getEmail() { return this.email;}
-	public int getPhone() { return this.phone;}
-	public Location getAddress() { return this.address;}
 	public String getHours() { return this.openingHours;}
 	
 	public boolean isPartner(BikeProvider p) {
