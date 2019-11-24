677c93ccd4af27bd322a2b28cc8645848ecffe48
UoEdinburghSato <57924218+UoEdinburghSato@users.noreply.github.com>
2019-11-24 00:14:44 +0000

    search method -> static, and it will return bike list which size is <=n

--- Class and Test/Quote.java
+++ Class and Test/Quote.java
@@ -11,36 +11,34 @@ public class Quote {
         this.setDeposite(deposite);
     }
 
-    public Bike getBike() {
-        return bike;
-    }
+    public Bike getBike() { return bike; }
 
-    public void setBike(Bike bike) {
-        this.bike = bike;
-    }
+    public void setBike(Bike bike) { this.bike = bike; }
 
-    public BigDecimal getDeposite() {
-        return deposite;
-    }
+    public BigDecimal getDeposite() { return deposite; }
 
-    public void setDeposite(BigDecimal deposite) {
-        this.deposite = deposite;
-    }
+    public void setDeposite(BigDecimal deposite) { this.deposite = deposite; }
+    
     
     @SuppressWarnings("unchecked")
-    public List<Bike> searchBike (Condition c, BikeList bikes){
+    public static List<Bike> searchBike (Condition c, BikeList bikes){
         List <Bike> results = new ArrayList<> ();
-        for (Bike b: (List <Bike>)bikes.bikes.keys()) {
+        for (Bike b: (List<Bike>)BikeList.getBikes().keys()) {
             if (c.getMaxPrice().compareTo(b.getPrice()) > 0 &&
                 b.getPrice().compareTo(c.getMinPrice()) > 0 &&
-                c.getProvider() == b.getProvider() &&
-                c.getType() == b.getType() &&
+                c.getProvider().getName() == b.getProvider().getName() &&
+                c.getType().getType() == b.getType().getType() &&
                 c.getLocation().isNearTo(b.getProvider().getAddress()) &&
-                b.isAvail(c.getDate())  )
+                b.isAvail(c.getDate()) )
             {
                 results.add(b);
             }
         }
+        
+        if (results.size() > c.getNumber()) {
+            return results.subList(0, c.getNumber());
+        }
+        
         return results;
     }
     
