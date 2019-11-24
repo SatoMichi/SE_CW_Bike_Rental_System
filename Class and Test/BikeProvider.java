ee54c756bd36066cff16e55bb6e4e92ef73aedee
UoEdinburghSato <57924218+UoEdinburghSato@users.noreply.github.com>
2019-11-24 00:11:31 +0000

    minor change due to BikeList become static

--- Class and Test/BikeProvider.java
+++ Class and Test/BikeProvider.java
@@ -1,29 +1,29 @@
 import java.util.List;
 
 public class BikeProvider extends Account {
-	private String openingHours;
-	private List<BikeProvider> partners;
-	
-	public BikeProvider(String name, String email, int phone, Location address, String openingHours) {
-		super(name, address, phone, email);
-		this.openingHours = openingHours;
-	}
-	
-	public void addPartner(BikeProvider p) { partners.add(p);}
-	public String getHours() { return this.openingHours;}
-	
-	public boolean isPartner(BikeProvider p) {
-		for (BikeProvider partner : partners) {
-			if (p == partner) return true;
-		}
-		return false;
-	}
-	
-	public void register(Bike b, int count, BikeList list) {
-		list.bikes.put(b, count);
-	}
-	
-	public void unregister(Bike b, BikeList list) {
-		list.bikes.remove(b);
-	}
+    private String openingHours;
+    private List<BikeProvider> partners;
+    
+    public BikeProvider(String name, String email, int phone, Location address, String openingHours) {
+        super(name, address, phone, email);
+        this.openingHours = openingHours;
+    }
+    
+    public void addPartner(BikeProvider p) { partners.add(p);}
+    public String getHours() { return this.openingHours;}
+    
+    public boolean isPartner(BikeProvider p) {
+        for (BikeProvider partner : partners) {
+            if (p == partner) return true;
+        }
+        return false;
+    }
+    
+    public void register(Bike b, int count) {
+        BikeList.getBikes().put(b, count);
+    }
+    
+    public void unregister(Bike b, BikeList list) {
+        BikeList.getBikes().remove(b);
+    }
 }
