41d8765adfaa5e0b04ff5c50f780d131cfbb2766
Zspirates96 <32324812+Zspirates96@users.noreply.github.com>
2019-11-25 20:36:50 +0000

    Update LinearDepreciation.java

--- Class and Test/LinearDepreciation.java
+++ Class and Test/LinearDepreciation.java
@@ -3,20 +3,10 @@ import java.time.LocalDate;
 
 public class LinearDepreciation implements ValuationPolicy {
     
-    private BigDecimal rate;
-    
-    public LinearDepreciation(BigDecimal rate) {
-        setRate(rate);
-    }
-    
-    
-    public BigDecimal getRate() { return rate; }
-
-    public void setRate(BigDecimal rate) { this.rate = rate; }
-
     @Override
     public BigDecimal calculateValue(Bike bike, LocalDate date) {
         BigDecimal originalV = bike.getType().getReplacementValue();
+        BigDecimal rate = new BigDecimal(bike.getProvider().getRate());
         BigDecimal year = new BigDecimal(date.getYear() - bike.getRegDate().getYear());
         BigDecimal Value = originalV.subtract(year.multiply(rate).multiply(originalV));
         return Value;
