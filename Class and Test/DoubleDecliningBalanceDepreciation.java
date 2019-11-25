725e89e5cf74d7502b1819e41766cd9cec6291b9
Zspirates96 <32324812+Zspirates96@users.noreply.github.com>
2019-11-25 20:36:35 +0000

    Update DoubleDecliningBalanceDepreciation.java

--- Class and Test/DoubleDecliningBalanceDepreciation.java
+++ Class and Test/DoubleDecliningBalanceDepreciation.java
@@ -2,20 +2,10 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class DoubleDecliningBalanceDepreciation implements ValuationPolicy {
-
-    private BigDecimal rate;
-    
-    public DoubleDecliningBalanceDepreciation(BigDecimal rate) {
-        setRate(rate);
-    }
-    
-    public BigDecimal getRate() { return rate; }
-
-    public void setRate(BigDecimal rate) { this.rate = rate; } 
-    
     @Override
     public BigDecimal calculateValue(Bike bike, LocalDate date) {
         BigDecimal originalV = bike.getType().getReplacementValue();
+        BigDecimal rate = new BigDecimal(bike.getProvider().getRate());
         int year = date.getYear() - bike.getRegDate().getYear();
         // Value = OriginalV * (1-2*rate)^year
         BigDecimal Value = originalV.multiply( BigDecimal.ONE.subtract( rate.add(rate) ).pow(year) );
