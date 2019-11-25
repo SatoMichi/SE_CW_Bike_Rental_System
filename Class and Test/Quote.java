74c64bd5824f44cfa0bad9e6be89c37107ae6083
UoEdinburghSato <57924218+UoEdinburghSato@users.noreply.github.com>
2019-11-25 20:52:38 +0000

    Quote become Wrapper class of Bike

--- Class and Test/Quote.java
+++ Class and Test/Quote.java
@@ -18,27 +18,5 @@ public class Quote {
     public BigDecimal getDeposite() { return deposite; }
 
     public void setDeposite(BigDecimal deposite) { this.deposite = deposite; }
-    
-    
-    @SuppressWarnings("unchecked")
-    public static List<Bike> searchBike (Condition c){
-        List <Bike> results = new ArrayList<> ();
-        for (Bike b: (List<Bike>)BikeList.getBikes().keys()) {
-            if (c.getMaxPrice().compareTo(b.getPrice()) > 0 &&
-                b.getPrice().compareTo(c.getMinPrice()) > 0 &&
-                c.getProvider().getName() == b.getProvider().getName() &&
-                c.getType().getType() == b.getType().getType() &&
-                c.getLocation().isNearTo(b.getProvider().getAddress()) &&
-                b.isAvail(c.getDate()) )
-            {
-                results.add(b);
-            }
-        }
-        
-        if (results.size() > c.getNumber()) {
-            return results.subList(0, c.getNumber());
-        }
-        
-        return results;
-    }   
+      
 }
