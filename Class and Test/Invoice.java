51e1b2ea5d33fca30b70d8372934fbd9278964cc
UoEdinburghSato <57924218+UoEdinburghSato@users.noreply.github.com>
2019-11-25 15:38:06 +0000

    Updated for dealing with more than one bike booked

--- Class and Test/Invoice.java
+++ Class and Test/Invoice.java
@@ -1,17 +1,17 @@
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 public class Invoice {
     
-    private Bike bike;
+    private List <Bike> bikes;
     private BigDecimal price;
     private LocalDate date;
     private Customer customer;
     private int ordernumber;
     
-    public Invoice(Bike bike, BigDecimal price, LocalDate date, Customer customer, int ordernumber) {
-        super();
-        this.bike = bike;
+    public Invoice(List<Bike> bikes, BigDecimal price, LocalDate date, Customer customer, int ordernumber) {
+        this.bikes = bikes;
         this.price = price;
         this.date = date;
         this.customer = customer;
@@ -19,10 +19,14 @@ public class Invoice {
     }
     
     public String toString() {
+        String bikesInfo = "";
+        for (Bike b: bikes) {
+            bikesInfo += b.toString();
+        }
         return ("OrderNumber: " + ordernumber + 
                 "\nDate: " + date +
                 "\nCustomer Name: " + customer.toString() +
-                "\nBiked booked is " + bike.toString() +
+                "\nBikes booked is " + bikesInfo +
                 "\nPrice is " + price.toString() + "\n");
     }
 }
