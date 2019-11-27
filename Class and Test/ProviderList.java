22613d50837ca78ad799c8a4bd181b66dfbb7018
Zspirates96 <32324812+Zspirates96@users.noreply.github.com>
2019-11-27 16:21:23 +0000

    Update ProviderList.java

--- Class and Test/ProviderList.java
+++ Class and Test/ProviderList.java
@@ -3,13 +3,4 @@ import java.util.ArrayList;
 
 public class ProviderList {
     protected static List<BikeProvider> providers = new ArrayList<BikeProvider>();
-    
-    public void increaseCount (BikeProvider p) {
-        int n  = providers.get(p) + 1;
-        providers.put(p, n);
-    }
-    public void decreaseCount (BikeProvider p) {
-        int n  = providers.get(p) - 1;
-        providers.put(p, n);
-    }
 }
