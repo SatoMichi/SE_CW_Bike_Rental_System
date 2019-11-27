5608b3123f8beec6aafd8e2cd0974e3b2aa91e42
Zspirates96 <32324812+Zspirates96@users.noreply.github.com>
2019-11-27 19:25:39 +0000

    Update Booking.java

--- Class and Test/Booking.java
+++ Class and Test/Booking.java
@@ -43,6 +43,9 @@ public class Booking {
             if (this.delivery) {
                 delivery.scheduleDelivery(b, p.getAddress(), this.customer.getAddress(), date.getEnd());
             }
+	    else {
+	        b.onDropoff();
+	    }
         }
 	}
 	
